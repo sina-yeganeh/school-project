c1c323a2df53124c77508935e893cf767430c3d5
Sina yeganeh <71815150+sina-yeganeh@users.noreply.github.com>
2021-05-05 15:16:11 +0430

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,14 @@
+ROOT_NAME = "Card Game"
+ROOT_BACKGROUND = "balck"
+WINDOW_SIZE = "305x305"
+FONT = "Consolas"
+TEXT_COLOR = None
+TEXT_SIZE = 16
+BACKGROUND_BUTTON_COLOR = "white"
+IMAGE_LIST_NAME = [
+    "./images/cat.png",
+    "./images/covid.png",
+    "./images/mask.png",
+    "./images/python.png",
+    "./images/finish.png"
+]
